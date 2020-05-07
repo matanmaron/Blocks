e090dcfb9fa828066f34ae409dd0ce9426c6ae6a
Matan Maron <matanmaron@gmail.com>
2020-05-07 19:42:34 +0300

    a full voxel cube

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    private void Start()
+    {
+        int vertexIndex = 0;
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triengles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                int triangleIndex = VoxelData.voxelTris[i, j];
+                vertices.Add(VoxelData.voxelVerts[triangleIndex]);
+
+                triengles.Add(vertexIndex);
+
+                uvs.Add(Vector2.zero);
+
+                vertexIndex++;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triengles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -13,4 +13,14 @@ public static class VoxelData
         new Vector3(1f,1f,1f),
         new Vector3(0f,1f,1f)
     };
+
+    public static readonly int[,] voxelTris = new int[6, 6]
+    {
+        {0,3,1,1,3,2}, //back face
+        {5,6,4,4,6,7}, //front face
+        {3,7,2,2,7,6}, //top face
+        {1,5,0,0,5,4}, //bottom face
+        {4,7,0,0,7,3}, //left face
+        {1,2,5,5,2,6} //right face
+    };
 }
