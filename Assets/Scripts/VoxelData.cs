66ef0469c03f66241ca4548f30063fb3b638fb56
Matan Maron <matanmaron@gmail.com>
2020-05-07 19:19:13 +0300

    Initial commit

---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class VoxelData
+{
+    public static readonly Vector3[] voxelVerts = new Vector3[8]
+    {
+        new Vector3(0f,0f,0f),
+        new Vector3(1f,0f,0f),
+        new Vector3(1f,1f,0f),
+        new Vector3(0f,1f,0f),
+        new Vector3(0f,0f,1f),
+        new Vector3(1f,0f,1f),
+        new Vector3(1f,1f,1f),
+        new Vector3(0f,1f,1f)
+    };
+}
